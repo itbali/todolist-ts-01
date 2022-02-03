f00c54cb336acb04b33ddf6c7d2d110158f87c1c
itbali <alex.itbali@gmail.com>
2022-02-03 22:53:55 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist";
+
+
+function App() {
+
+    let tasks1 = [
+        {id: 1, title: "HTML & CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Redux", isDone: false},
+    ]
+
+    function removeTask (id: number) {
+        tasks1 = tasks1.filter( t=> t.id !== id
+        )
+        console.log(tasks1)
+    }
+
+    return (
+        <div className="App">
+            <Todolist
+                title="What to learn"
+                tasks={tasks1}
+                removeTask={removeTask}
+            />
+        </div>
+    );
+}
+
+export default App;
